448f077d4a417ff65a72ecd86ae3548937f1f405
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-06-16 23:17:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+import { Accelerometer, Microbit } from "microbyte";
+const mb = new Microbit([Accelerometer]);
---
+++ index.ts
@@ -0,0 +1,3 @@
+import { Accelerometer, Microbit } from "microbyte";
+
+const mb = new Microbit([Accelerometer]);
