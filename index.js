e15bc3e7fd05e961a3df5a7fee1c44cad6ab3c0b
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-06-16 23:37:43 +0200

    Auto-reloading

--- index.js
+++
@@ -1,2 +0,0 @@
-import { Accelerometer, Microbit } from "microbyte";
-const mb = new Microbit([Accelerometer]);
--- index.ts
+++
@@ -1,3 +0,0 @@
-import { Accelerometer, Microbit } from "microbyte";
-
-const mb = new Microbit([Accelerometer]);
