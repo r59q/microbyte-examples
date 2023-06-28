706bae9dc2f7a297cc41aad53f7c759e61ecfdd7
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-06-29 00:55:43 +0200

    Added connection by name

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     import LogMessages from './LogMessages.svelte';
     import SectionContainer from './SectionContainer.svelte';
+    import BluetoothByNameConnection from './examples/BluetoothByNameConnection.svelte';
     import BluetoothConnection from './examples/BluetoothConnection.svelte';
 </script>
 
 <div class="column">
     <SectionContainer title="Bluetooth connection">
         <BluetoothConnection />
+    </SectionContainer>
+    <SectionContainer title="Bluetooth connection using name">
+        <BluetoothByNameConnection />
     </SectionContainer>
     <LogMessages />
 </div>
---
+++ src/examples/BluetoothByNameConnection.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { Microbit } from 'microbyte';
+    import { addMessage } from '../util';
+
+    let microbitName = '';
+
+    const onConnectButtonClick = () => {
+        if (microbitName.length != 5) {
+            addMessage(
+                `Error: Microbit names are expected to be of length 5, saw length ${microbitName.length}`
+            );
+            return;
+        }
+
+        addMessage('Connecting...');
+
+        const microbit = new Microbit();
+        microbit.requestBluetooth(
+            microbitName,
+            () => {
+                addMessage('Connection successful!');
+            },
+            (reason) => {
+                addMessage('Failed to connect!');
+                addMessage(reason);
+            }
+        );
+    };
+</script>
+
+<p>Connect via bluetooth to a microbit.</p>
+<p>Enter the name of microbit</p>
+<input bind:value={microbitName} />
+<button on:click={onConnectButtonClick}>Click me to connect</button>
